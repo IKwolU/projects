17f329e14f143a8f6fd280ba3f89b09b87746909
DVlitvinenko <124286708+DVlitvinenko@users.noreply.github.com>
2024-04-03 11:52:11 +0300

    build

--- garage/frontend/@/lib/utils.ts
+++ garage/frontend/@/lib/utils.ts
@@ -4,11 +4,9 @@ import {
   DayOfWeek,
   FuelType,
   TransmissionType,
-  Working_hours5,
 } from "../../src/api-client";
 import { twMerge } from "tailwind-merge";
 import { format } from "date-fns";
-import { forEach } from "ramda";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -54,25 +52,6 @@ export const formatRoubles = (amount: number) =>
 export function formatWorkingTime(hours: number, minutes: number) {
   return `${format(new Date(2000, 0, 0, hours, minutes), "HH:mm")}`;
 }
-
-export const CheckWorkingHours = (workingHours: Working_hours5[]) => {
-  const monday = `${workingHours[0].start?.hours}:${workingHours[0].start?.minutes}:${workingHours[0].end?.hours}:${workingHours[0].end?.minutes}`;
-  const tuesday = `${workingHours[1].start?.hours}:${workingHours[1].start?.minutes}:${workingHours[1].end?.hours}:${workingHours[1].end?.minutes}`;
-  const wednesday = `${workingHours[2].start?.hours}:${workingHours[2].start?.minutes}:${workingHours[2].end?.hours}:${workingHours[2].end?.minutes}`;
-  const thursday = `${workingHours[3].start?.hours}:${workingHours[3].start?.minutes}:${workingHours[3].end?.hours}:${workingHours[3].end?.minutes}`;
-  const friday = `${workingHours[4].start?.hours}:${workingHours[4].start?.minutes}:${workingHours[4].end?.hours}:${workingHours[4].end?.minutes}`;
-
-  if (
-    monday === tuesday &&
-    tuesday === wednesday &&
-    wednesday === thursday &&
-    thursday === friday
-  ) {
-    return false;
-  } else {
-    return true;
-  }
-};
 
 export const getDayOfWeekDisplayName = (day: DayOfWeek) => {
   const daysOfWeek = {

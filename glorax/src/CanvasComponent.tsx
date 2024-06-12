d66006b52b1050e436972222e4e8dd97b7e5a5ee
DVlitvinenko <124286708+DVlitvinenko@users.noreply.github.com>
2024-06-12 10:45:26 +0300

    new map

--- glorax/src/CanvasComponent.tsx
+++ glorax/src/CanvasComponent.tsx
@@ -372,7 +372,7 @@ function CanvasComponent() {
 
         <OBJModel
           scale={[1, 1, 1]}
-          file="/models/1106editmap.gltf"
+          file="/models/12.06map.gltf"
           texture={ColorToTexture(new THREE.Color(65, 105, 225))}
           position={[0, -4, 0]}
           opacity={0.93}
